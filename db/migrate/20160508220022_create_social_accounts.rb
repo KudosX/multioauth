d5db3c62d250f58509a5d5c577dfc6a2942c722f
KudosX <max@shitbrains.com>
2016-05-08 16:17:00 -0700

    step 1 models

---
+++ db/migrate/20160508220022_create_social_accounts.rb
@@ -0,0 +1,21 @@
+class CreateSocialAccounts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :social_accounts do |t|
+      t.references :user, foreign_key: true
+      t.integer :user_id
+      t.string :name
+      t.string :provider, index: true
+      t.string :uid, index: true
+      t.string :location
+      t.string :image_url
+      t.string :url
+
+      t.timestamps null: false
+    end
+
+    add_index :social_accounts, :provider
+    add_index :social_accounts, :uid
+    add_index :social_accounts, :user_id
+    add_index :social_accounts, [:provider, :uid], unique: true
+  end
+end
